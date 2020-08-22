8f4170b10b439e6323359986f80f1cd10b10ba84
Debasis Dutta <64883709+debasis006@users.noreply.github.com>
2020-08-22 12:06:45 +0530

    Add files via upload
    Open your editor then write the code which is given bellow in the link . By doing this run the code , get the timer and press the enter key and boom see the magic.😎😎😎

---
+++ Restart.py
@@ -0,0 +1,14 @@
+import os
+
+class Restart(object):
+    def __init__(self, timer=0):
+        self.timer = int(timer)
+    def __repr__(self):
+        os.system(f"shutdown -r -t {self.timer * 60}")
+        return str(f"system will restart in {self.timer} minutes!")
+if __name__ == "__main__":
+    try:
+        restart_in = int (input("Minutes untill restart?: "))
+        print(Restart(restart_in))
+    except:
+        print(Restart())
